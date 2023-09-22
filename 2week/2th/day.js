42ed1c762cd096293965db48ebde44dab20f2b47
hyunhokimm <whoaukim@gmail.com>
2023-09-22 20:34:36 +0900

    switch문

---
+++ 2week/2th/day.js
@@ -0,0 +1,24 @@
+function dayCheck(day) {
+    switch (day) {
+        case '월요일':
+            return '월요일';
+            break;
+        case '화요일':
+            return '화요일';
+            break;
+        case '수요일':
+            return '수요일';
+            break;
+        case '목요일':
+            return '목요일';
+            break;
+        case '금요일':
+            return '금요일';
+            break;
+        default:
+            return '휴일'
+            break
+    }
+}
+
+console.log(dayCheck('토요일'))

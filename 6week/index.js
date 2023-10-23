787284d7b87e7b1c24cf4d1c7dbef6a954b49c11
hyunhokimm <whoaukim@gmail.com>
2023-10-23 21:36:50 +0900

    multer

--- 6week/index.js
+++ 6week/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 8000;
+const multer = require('multer');
+const path = require('path');
+const upload = multer({ dest: 'uploads/' });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -10,9 +13,19 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/profile', (req, res) => {
+  res.render('fileUploads');
+});
+
 app.post('/userCreate', (req, res) => {
   const user = req.body;
   console.log(user);
+});
+
+app.post('/multerFile', upload.single('file'), (req, res, next) => {
+  console.log(req.file);
+  console.log(req.body);
+  res.send('ok');
 });
 
 app.listen(PORT, () => {

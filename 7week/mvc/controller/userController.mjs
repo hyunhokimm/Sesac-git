2b4ee66bf019ad08871d5c9fb29f208186d35146
hyunhokimm <whoaukim@gmail.com>
2023-10-27 18:44:15 +0900

    home work

--- 7week/mvc/controller/userController.mjs
+++ 7week/mvc/controller/userController.mjs
@@ -1,11 +1,29 @@
+let user = [
+  {
+    dbId: 'kim',
+    dbPw: '111',
+  },
+  {
+    dbId: 'lee',
+    dbPw: '222',
+  },
+  {
+    dbId: 'oh',
+    dbPw: '333',
+  },
+];
+
 export const userCheck = (req) => {
-  const dbId = 'kim';
-  const dbPw = '1111';
-  if (dbId === req.id && dbPw === req.password) {
-    return '신원이 확인되었습니다.';
-  } else {
-    return '신원 확인이 필요합니다.';
+  for (let i = 0; i < user.length; i++) {
+    console.log(i);
+    console.log(user[i].dbId, user[i].dbPw);
+    console.log(req.id, req.password);
+    if (user[i].dbId === req.id && user[i].dbPw === req.password) {
+      return '신원이 확인되었습니다.';
+    }
   }
+  return '신원 확인이 필요합니다.';
 };
+
 export const userAll = () => {};
 export const userOne = () => {};
